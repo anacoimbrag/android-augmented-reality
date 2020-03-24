d1671c66ab7a2ca346083716d16c2e2bafe2ea33
Ana Coimbra <ana.coimbra.gomes@gmail.com>
2020-03-24 11:41:28 -0300

    Initial arcore config

--- app/src/main/java/dev/anacoimbra/androidaugmentedreality/MainActivity.kt
+++ app/src/main/java/dev/anacoimbra/androidaugmentedreality/MainActivity.kt
@@ -1,10 +1,12 @@
 package dev.anacoimbra.androidaugmentedreality
 
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.recyclerview.widget.DividerItemDecoration
+import com.google.ar.core.ArCoreApk
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +15,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         mainItems.adapter = MainAdapter()
+        maybeEnableButtons()
+    }
+
+    private fun maybeEnableButtons() {
+        val availability = ArCoreApk.getInstance().checkAvailability(this)
+        if (availability.isTransient)
+            Handler().postDelayed({ maybeEnableButtons() }, 200)
+
+        if (availability.isSupported) mainItems.visibility = View.VISIBLE
+        else mainItems.visibility = View.GONE
     }
 }
