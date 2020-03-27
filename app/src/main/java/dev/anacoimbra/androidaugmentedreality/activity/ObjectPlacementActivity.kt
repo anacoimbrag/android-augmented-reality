6705a061874ae3679ea74cbfa2bd0901a768a1f2
Ana Coimbra <ana.coimbra.gomes@gmail.com>
2020-03-27 18:59:11 -0300

    wip: setup target on plane renderer

--- app/src/main/java/dev/anacoimbra/androidaugmentedreality/activity/ObjectPlacementActivity.kt
+++ app/src/main/java/dev/anacoimbra/androidaugmentedreality/activity/ObjectPlacementActivity.kt
@@ -3,19 +3,29 @@ package dev.anacoimbra.androidaugmentedreality.activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import com.google.ar.core.Plane
+import com.google.ar.sceneform.Node
+import com.google.ar.sceneform.math.Vector3
+import com.google.ar.sceneform.rendering.*
 import com.google.ar.sceneform.ux.ArFragment
+import dev.anacoimbra.androidaugmentedreality.R
 import dev.anacoimbra.androidaugmentedreality.helpers.loadRenderable
 
 class ObjectPlacementActivity : BaseArActivity() {
 
     override val arFragment = ArFragment()
 
+    private val node = Node()
+    private lateinit var floorRenderable: ModelRenderable
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         replaceFragment {
             setupPlaneTap()
+            setupUpdate()
         }
 
+        loadModel()
         loadRenderable(this, onSuccess = ::renderable.setter)
     }
 
@@ -26,6 +36,41 @@ class ObjectPlacementActivity : BaseArActivity() {
             placeNode(hitResult.createAnchor())
             arFragment.setOnTapArPlaneListener(null)
         }
+    }
+
+    private fun setupUpdate() {
+        arFragment.arSceneView.scene.addOnUpdateListener {
+            if (!::floorRenderable.isInitialized) return@addOnUpdateListener
+            val frame = arFragment.arSceneView.arFrame ?: return@addOnUpdateListener
+            val planes = frame.getUpdatedTrackables(Plane::class.java)
+            planes.forEach { plane ->
+                val pose = plane.centerPose
+                node.localPosition = Vector3(pose.tx(), pose.ty(), pose.tz())
+
+                node.renderable = floorRenderable
+                node.setParent(arFragment.arSceneView.scene)
+            }
+        }
+    }
+
+    private fun loadModel() {
+        val sampler =
+            Texture.Sampler.builder()
+                .setMagFilter(Texture.Sampler.MagFilter.LINEAR)
+                .setMinFilter(Texture.Sampler.MinFilter.LINEAR)
+                .setWrapMode(Texture.Sampler.WrapMode.REPEAT)
+                .build()
+        Texture.builder()
+            .setSource(this, R.drawable.target)
+            .setSampler(sampler)
+            .build()
+            .thenAccept { texture ->
+                MaterialFactory.makeOpaqueWithTexture(this, texture)
+                    .thenAccept { material ->
+                        floorRenderable =
+                            ShapeFactory.makeCylinder(0.1f, 0.00001f, Vector3(0f, 0f, 0f), material)
+                    }
+            }
     }
 
     companion object {
